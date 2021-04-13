c0c9274dc7898a2546d3561b14935278337673b0
Yosef <shmuli5770@gmail.com>
2021-04-13 10:07:45 -0400

    We tried :(

--- src/index.js
+++ src/index.js
@@ -1,6 +1,35 @@
-const fullname = document.getElementById("fullname");
-console.log("CONTENT NOT YET LOADED!", fullname); //what will fullname evaluate to?
+const street = document.getElementById('street')
+const city = document.getElementById('city')
+const state = document.getElementById('state')
+const postcode = document.getElementById('postcode')
+const phone = document.getElementById('phone')
+const cell = document.getElementById('cell')
+const dob = document.getElementById('date_of_birth')
+const picture = document.getElementById('profile_picture')
+const fullname = document.getElementById('fullname')
+const email = document.getElementById('email')
+const button = document.querySelector('.btn.btn-primary')
 
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("CONTENT LOADED!");
-});
+button.addEventListener('click', getData)
+console.log('line 14')
+
+function getData() {
+  fetch('https://randomuser.me/api/')
+  .then(res => res.json())
+  .then(object => {
+    // object.results.forEach(element => {
+    //   if (typeof(element) === 'object') {
+    //     for (const key of object) {
+    //       let allInnerKeys = Object.keys(key)
+    //       console.log(allInnerKeys)
+    //     }
+    //   } else {
+    //     let allKeys = Object.keys(element)
+    //     console.log(allKeys)
+    //   }
+    // });
+    street.innerText = object.results[0].location.street.
+    cell.innerText = object.results[0].cell
+
+  })
+}
