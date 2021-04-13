7f806e3a60f4f9acef1b12da0d6ac56829de21b3
Yosef <shmuli5770@gmail.com>
2021-04-13 14:33:43 -0400

    Correctly populating page, added clear function so new objects don't append to currently displayed.

--- src/index.js
+++ src/index.js
@@ -1,35 +1,40 @@
-const street = document.getElementById('street')
-const city = document.getElementById('city')
-const state = document.getElementById('state')
-const postcode = document.getElementById('postcode')
-const phone = document.getElementById('phone')
-const cell = document.getElementById('cell')
-const dob = document.getElementById('date_of_birth')
-const picture = document.getElementById('profile_picture')
-const fullname = document.getElementById('fullname')
-const email = document.getElementById('email')
-const button = document.querySelector('.btn.btn-primary')
+const elements = {}
+elements.street = document.getElementById('street')
+elements.city = document.getElementById('city')
+elements.state = document.getElementById('state')
+elements.postcode = document.getElementById('postcode')
+elements.phone = document.getElementById('phone')
+elements.cell = document.getElementById('cell')
+elements.dob = document.getElementById('date_of_birth')
+elements.picture = document.getElementById('profile_picture')
+elements.fullname = document.getElementById('fullname')
+elements.email = document.getElementById('email')
+elements.button = document.querySelector('.btn.btn-primary')
 
-button.addEventListener('click', getData)
-console.log('line 14')
+elements.button.addEventListener('click', getData)
 
 function getData() {
+  for (element in elements) {
+    if (element !== 'button') {
+      elements[element].innerText = ''
+    }
+  }
+
   fetch('https://randomuser.me/api/')
   .then(res => res.json())
   .then(object => {
-    // object.results.forEach(element => {
-    //   if (typeof(element) === 'object') {
-    //     for (const key of object) {
-    //       let allInnerKeys = Object.keys(key)
-    //       console.log(allInnerKeys)
-    //     }
-    //   } else {
-    //     let allKeys = Object.keys(element)
-    //     console.log(allKeys)
-    //   }
-    // });
-    street.innerText = object.results[0].location.street.
-    cell.innerText = object.results[0].cell
-
+    const usableData = object.results[0]
+    let streetObj = usableData.location.street
+    let nameObj = usableData.name
+    elements.street.append(streetObj.number + ' ' + streetObj.name)
+    elements.fullname.append(`${nameObj.title} ${nameObj.first} ${nameObj.last}`)
+    elements.cell.append(usableData.cell)
+    elements.city.append(usableData.location.city)
+    elements.state.append(usableData.location.state)
+    elements.postcode.append(usableData.location.postcode)
+    elements.phone.append(usableData.phone)
+    elements.dob.append(usableData.dob.date.slice(0,10))
+    elements.email.append(usableData.email)
+    elements.picture.src = (usableData.picture.large)
   })
 }
